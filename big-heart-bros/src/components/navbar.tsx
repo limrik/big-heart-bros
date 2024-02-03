4d7289896276b2e916024761a7107d7fef6fe870
Clifton <yeoclifton@gmail.com>
2024-02-04 03:46:39 +0800

    updated nav bar

--- big-heart-bros/src/components/navbar.tsx
+++ big-heart-bros/src/components/navbar.tsx
@@ -1,36 +1,45 @@
 "use client";
 import Link from "next/link";
+import { Button } from "./ui/button";
+import { PersonIcon } from "@radix-ui/react-icons"
 
 export default function Navbar() {
   return (
-    <div className="bg-[#8B0000] h-16 flex flex-row items-center justify-center text-white grid-cols-2">
-      <div className="mr-16 font-semibold font-poppin text-lg">
+    <div className="bg-[#8B0000]">
+      <div className="w-5/6 h-16 flex flex-row items-center justify-center text-white mx-auto">
+      <div className="mr-auto font-semibold font-poppin text-lg">
         BigHeartBros
       </div>
-      <Link
-        className="mx-8 font-poppins cursor-pointer hover:text-blue-300"
-        href="/home"
-      >
-        Home
-      </Link>
-      <Link
-        className="mx-8 font-poppins cursor-pointer hover:text-blue-300"
-        href="/about"
-      >
-        About Us
-      </Link>
-      <Link
-        className="mx-8 font-poppins cursor-pointer hover:text-blue-300"
-        href="/user-dashboard"
-      >
-        Dashboard
-      </Link>
-      <Link
-        className="mx-8 font-poppins cursor-pointer hover:text-blue-300"
-        href="/profile"
-      >
-        Profile
-      </Link>
+      <div className="flex flex-row space-x-8 items-center">
+        <Link
+          className="font-poppins cursor-pointer hover:text-blue-300"
+          href="/home"
+        >
+          Home
+        </Link>
+        <Link
+          className="font-poppins cursor-pointer hover:text-blue-300"
+          href="/about"
+        >
+          About Us
+        </Link>
+        <Link
+          className="font-poppins cursor-pointer hover:text-blue-300"
+          href="/user-dashboard"
+        >
+          Dashboard
+        </Link>
+        <Button className="bg-[#ff5656] rounded-2xl hover:bg-[#ff8585]">
+          <PersonIcon className="w-4 h-4 mr-2" />
+          <Link
+            className="font-poppins cursor-pointer"
+            href="/profile"
+          >
+            Sign In
+          </Link>
+        </Button>
+      </div>
+      </div>
     </div>
   );
 }
