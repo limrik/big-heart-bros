80d90702943a714a52c119481fc0df9110466fe9
Clifton <yeoclifton@gmail.com>
2024-02-03 23:56:58 +0800

    add pages

---
+++ big-heart-bros/src/app/profile/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const About: React.FC = () => {
+    return (
+        <div>
+            <h1>Profile</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
+                mauris eu nisl ultrices, vitae tincidunt nunc tincidunt. Nulla facilisi.
+                Nullam auctor, nunc id aliquam tincidunt, nunc elit aliquet nunc, nec
+                luctus nunc nunc id nunc. Sed in semper nunc. Sed auctor, nunc id
+                aliquam tincidunt, nunc elit aliquet nunc, nec luctus nunc nunc id nunc.
+                Sed in semper nunc.
+            </p>
+            <p>
+                Sed auctor, nunc id aliquam tincidunt, nunc elit aliquet nunc, nec
+                luctus nunc nunc id nunc. Sed in semper nunc. Sed auctor, nunc id
+                aliquam tincidunt, nunc elit aliquet nunc, nec luctus nunc nunc id nunc.
+                Sed in semper nunc.
+            </p>
+        </div>
+    );
+};
+
+export default About;
