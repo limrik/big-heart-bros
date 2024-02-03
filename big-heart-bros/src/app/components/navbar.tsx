d927da470961f8f5eabbccd1baf109d9691497f3
Clifton <yeoclifton@gmail.com>
2024-02-03 23:56:25 +0800

    refactor nav bar

--- big-heart-bros/src/app/components/navbar.tsx
+++ big-heart-bros/src/app/components/navbar.tsx
@@ -1,28 +1,14 @@
 "use client";
 
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-} from "@/app/components/navigation-menu";
+import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    // <main>
     <div className="bg-[#8B0000] h-12 flex flex-row items-center justify-center text-white grid-cols-2">
-      <div className="mr-16 font-semibold font-poppins">BigHeartBros</div>
-      <div className="mx-8 font-poppins cursor-pointer hover:text-blue-300">
-        Home
-      </div>
-      <div className="mx-8 font-poppins cursor-pointer hover:text-blue-300">
-        <button onClick={() => null}>Sign Out</button>
-      </div>
+        <Link className="mx-8 font-poppins cursor-pointer hover:text-blue-300" href="/home">Home</Link>
+        <Link className="mx-8 font-poppins cursor-pointer hover:text-blue-300" href="/about">About Us</Link>
+        <Link className="mx-8 font-poppins cursor-pointer hover:text-blue-300" href="/user-dashboard">Dashboard</Link>
+        <Link className="mx-8 font-poppins cursor-pointer hover:text-blue-300" href="/profile">Profile</Link>
     </div>
-    // </main>
   );
 }
