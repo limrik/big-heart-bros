64b28e1851bbd960ef4eaf0a3a08b8fbc5818023
Clifton <yeoclifton@gmail.com>
2024-02-04 00:01:34 +0800

    add big heart bros to nav bar

--- big-heart-bros/src/app/components/navbar.tsx
+++ big-heart-bros/src/app/components/navbar.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 export default function Navbar() {
   return (
     <div className="bg-[#8B0000] h-12 flex flex-row items-center justify-center text-white grid-cols-2">
+        <div className="mr-16 font-semibold font-poppins">BigHeartBros</div>
         <Link className="mx-8 font-poppins cursor-pointer hover:text-blue-300" href="/home">Home</Link>
         <Link className="mx-8 font-poppins cursor-pointer hover:text-blue-300" href="/about">About Us</Link>
         <Link className="mx-8 font-poppins cursor-pointer hover:text-blue-300" href="/user-dashboard">Dashboard</Link>
