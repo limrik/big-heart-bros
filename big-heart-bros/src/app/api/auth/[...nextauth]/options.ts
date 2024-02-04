335e33cceede4f29aaa8e6c2eca51321cb20a48e
smellystank <ryan.peh.wye.kin.9795@gmail.com>
2024-02-04 17:53:46 +0800

    Auth (Google)
    - Google
    - Email (WIP)
    
    - Links to supabase

--- big-heart-bros/src/app/api/auth/[...nextauth]/options.ts
+++ big-heart-bros/src/app/api/auth/[...nextauth]/options.ts
@@ -1,36 +1,25 @@
 import type { NextAuthOptions } from "next-auth";
 import GitHubProvider from 'next-auth/providers/github'
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from 'next-auth/providers/google'
+import { SupabaseAdapter } from "@auth/supabase-adapter";
+import { Adapter } from "next-auth/adapters";
+import EmailProvider from "next-auth/providers/email";
 
 export const options: NextAuthOptions = {
 	providers: [
-		GitHubProvider({
-			clientId: process.env.GITHUB_ID as string,
-			clientSecret: process.env.GITHUB_SECRET as string,
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID as string,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
 		}),
-		CredentialsProvider({
-			name: "Credentials",
-			credentials : {
-				username: {
-					label: "Username:",
-					type: "text",
-					placeholder: "your-cool-username"
-				},
-				password: {
-					label: "Password:",
-					type: "password",
-					placeholder: "your-password-pants"
-				}
-			},
-			async authorize(credentials) { //can get data from database
-				const user = { id: "42", name: "nigg", password: "nigg"} //https://next-auth.js.org/configuration/providers/credentials
-
-				if (credentials?.username === user.name && credentials?.password === user.password) {
-					return user
-				} else {
-					return null
-				}
-			}
-		})
+		EmailProvider({
+			server: process.env.EMAIL_SERVER as string,
+			from: process.env.EMAIL_FROM as string
+		  }),
 	],
+	
+	adapter: SupabaseAdapter({
+		url: process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+		secret: process.env.SUPABASE_SERVICE_ROLE_KEY as string,
+	  }) as Adapter,
 }
