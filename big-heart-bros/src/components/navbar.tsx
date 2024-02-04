eb9ae00fe649f67a005e4710db12e457895155d1
Dayn <daynangzj@gmail.com>
2024-02-04 21:12:55 +0800

    Add work-in-progress profile view

--- big-heart-bros/src/components/navbar.tsx
+++ big-heart-bros/src/components/navbar.tsx
@@ -1,57 +1,54 @@
 "use client";
 import Link from "next/link";
 import { Button } from "./ui/button";
-import { PersonIcon } from "@radix-ui/react-icons"
+import { PersonIcon } from "@radix-ui/react-icons";
 
 export default function Navbar() {
   return (
     <div className="bg-[#8B0000]">
       <div className="w-5/6 h-16 flex flex-row items-center justify-center text-white mx-auto">
-      <div className="mr-auto font-semibold font-poppin text-lg">
-        BigHeartBros
-      </div>
-      <div className="flex flex-row space-x-8 items-center">
-        <Link
-          className="font-poppins cursor-pointer hover:text-blue-300"
-          href="/home"
-        >
-          Home
-        </Link>
-        <Link
-          className="font-poppins cursor-pointer hover:text-blue-300"
-          href="/about"
-        >
-          About Us
-        </Link>
-        <Link
-          className="font-poppins cursor-pointer hover:text-blue-300"
-          href="/user-dashboard"
-        >
-          Dashboard (user)
-        </Link>
-        
-        <Link
-          className="font-poppins cursor-pointer hover:text-blue-300"
-          href="/org-dashboard"
-        >
-          Dashboard (org)
-        </Link>
-        <Link
-          className="font-poppins cursor-pointer hover:text-blue-300"
-          href="/admin-dashboard"
-        >
-          Dashboard (admin)
-        </Link>
-        <Button className="bg-[#ff5656] rounded-2xl hover:bg-[#ff8585]">
-          <PersonIcon className="w-4 h-4 mr-2" />
+        <div className="mr-auto font-semibold font-poppin text-lg">
+          BigHeartBros
+        </div>
+        <div className="flex flex-row space-x-8 items-center">
           <Link
-            className="font-poppins cursor-pointer"
-            href="/profile"
+            className="font-poppins cursor-pointer hover:text-blue-300"
+            href="/home"
           >
-            Sign In
+            Home
           </Link>
-        </Button>
-      </div>
+          <Link
+            className="font-poppins cursor-pointer hover:text-blue-300"
+            href="/about"
+          >
+            About Us
+          </Link>
+          <Link
+            className="font-poppins cursor-pointer hover:text-blue-300"
+            href="/user-dashboard"
+          >
+            Dashboard (user)
+          </Link>
+
+          <Link
+            className="font-poppins cursor-pointer hover:text-blue-300"
+            href="/org-dashboard"
+          >
+            Dashboard (org)
+          </Link>
+          <Link
+            className="font-poppins cursor-pointer hover:text-blue-300"
+            href="/admin-dashboard"
+          >
+            Dashboard (admin)
+          </Link>
+          <Button className="bg-[#ff5656] rounded-2xl hover:bg-[#ff8585]">
+            <PersonIcon className="w-4 h-4 mr-2" />
+            <Link className="font-poppins cursor-pointer" href="/sign-in">
+              Sign In
+            </Link>
+          </Button>
+        </div>
       </div>
     </div>
   );
---
+++ big-heart-bros/src/components/profile.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import ProfilePhoto from "../app/assets/profile-photo.png";
+import { Separator } from "./ui/separator";
+
+export default function Profile() {
+  return (
+    <div className="p-2 px-4 flex items-center bg-white relative justify-center w-80">
+      <Image
+        className="rounded-full absolute left-1/2 transform -translate-y-24 -translate-x-1/2"
+        src={ProfilePhoto}
+        alt="profile-photo"
+        width={100}
+        height={100}
+      />
+      <div className="mt-20 items-center text-center">
+        {" "}
+        <p className="text-xl font-semibold mb-2">Omar Apollo</p>
+        <div className="text-center w-60 flex flex-row items-center justify-center">
+          <div>
+            <p className="text-sm text-gray-600">Total Events</p>
+            <p className="text-lg">5</p>
+          </div>
+          <Separator orientation="vertical" className="mx-2" />
+          <div>
+            <p className="text-sm text-gray-600">Total Hours</p>
+            <p className="text-lg">100</p>
+          </div>
+        </div>{" "}
+        <Separator className="my-2" />
+        <div className="text-left">
+          <p className="text-sm text-gray-600">Latest Activity</p>
+          <p className="text-sm">Handicraft Workshop</p>
+        </div>
+      </div>
+    </div>
+  );
+}
