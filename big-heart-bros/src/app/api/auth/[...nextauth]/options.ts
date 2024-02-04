c3321bd4147dcbaea3754f9bba42852ed0fe720f
smellystank <ryan.peh.wye.kin.9795@gmail.com>
2024-02-04 23:58:21 +0800

    Auth (Google, Github)

--- big-heart-bros/src/app/api/auth/[...nextauth]/options.ts
+++ big-heart-bros/src/app/api/auth/[...nextauth]/options.ts
@@ -1,6 +1,10 @@
 import type { NextAuthOptions } from "next-auth";
 import GitHubProvider from 'next-auth/providers/github'
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from 'next-auth/providers/google'
+import { SupabaseAdapter } from "@auth/supabase-adapter";
+import { Adapter } from "next-auth/adapters";
+import EmailProvider from "next-auth/providers/email";
 
 export const options: NextAuthOptions = {
 	providers: [
@@ -8,7 +12,11 @@ export const options: NextAuthOptions = {
 			clientId: process.env.GITHUB_ID as string,
 			clientSecret: process.env.GITHUB_SECRET as string,
 		}),
-		CredentialsProvider({
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID as string,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+		}),
+		/*CredentialsProvider({
 			name: "Credentials",
 			credentials : {
 				username: {
@@ -31,6 +39,10 @@ export const options: NextAuthOptions = {
 					return null
 				}
 			}
-		})
+		}) */
 	],
+	adapter: SupabaseAdapter({
+		url: process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+		secret: process.env.SUPABASE_SERVICE_ROLE_KEY as string,
+	  }) as Adapter,
 }
