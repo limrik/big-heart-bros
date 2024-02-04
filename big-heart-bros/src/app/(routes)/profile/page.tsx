c6599662e1687b6f9c0e099d1ca951c95f8138a9
smellystank <ryan.peh.wye.kin.9795@gmail.com>
2024-02-04 18:25:51 +0800

    updating auth

--- big-heart-bros/src/app/(routes)/profile/page.tsx
+++ big-heart-bros/src/app/(routes)/profile/page.tsx
@@ -1,27 +1,40 @@
+"use client"
 import React from 'react';
 import Navbar from '../../../components/navbar';
+import { useSession } from 'next-auth/react';
 
 const About: React.FC = () => {
+    const { data: session } = useSession();
+
     return (
         <div>
-            <Navbar></Navbar>
-            <h1>Profile</h1>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
-                mauris eu nisl ultrices, vitae tincidunt nunc tincidunt. Nulla facilisi.
-                Nullam auctor, nunc id aliquam tincidunt, nunc elit aliquet nunc, nec
-                luctus nunc nunc id nunc. Sed in semper nunc. Sed auctor, nunc id
-                aliquam tincidunt, nunc elit aliquet nunc, nec luctus nunc nunc id nunc.
-                Sed in semper nunc.
-            </p>
-            <p>
-                Sed auctor, nunc id aliquam tincidunt, nunc elit aliquet nunc, nec
-                luctus nunc nunc id nunc. Sed in semper nunc. Sed auctor, nunc id
-                aliquam tincidunt, nunc elit aliquet nunc, nec luctus nunc nunc id nunc.
-                Sed in semper nunc.
-            </p>
+            {session ? (
+                <div>
+                    <Navbar></Navbar>
+                    <h1>Profile</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
+                        mauris eu nisl ultrices, vitae tincidunt nunc tincidunt. Nulla facilisi.
+                        Nullam auctor, nunc id aliquam tincidunt, nunc elit aliquet nunc, nec
+                        luctus nunc nunc id nunc. Sed in semper nunc. Sed auctor, nunc id
+                        aliquam tincidunt, nunc elit aliquet nunc, nec luctus nunc nunc id nunc.
+                        Sed in semper nunc.
+                    </p>
+                    <p>
+                        Sed auctor, nunc id aliquam tincidunt, nunc elit aliquet nunc, nec
+                        luctus nunc nunc id nunc. Sed in semper nunc. Sed auctor, nunc id
+                        aliquam tincidunt, nunc elit aliquet nunc, nec luctus nunc nunc id nunc.
+                        Sed in semper nunc.
+                    </p>
+                </div>
+            ) : (
+                <div>
+                    <p>test</p>
+                </div>
+            )}
         </div>
     );
 };
 
 export default About;
+
