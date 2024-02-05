668a1d70957065e27bd33971c564685c4dc50c81
smellystank <ryan.peh.wye.kin.9795@gmail.com>
2024-02-05 14:25:35 +0800

    NextAuth with email implemented
    - Sign in
    - Will get email
    - click and it will redirect back to web app that is signed in
    - Information will be saved under next_auth table in supabase

--- big-heart-bros/src/app/api/auth/[...nextauth]/options.ts
+++ big-heart-bros/src/app/api/auth/[...nextauth]/options.ts
@@ -8,14 +8,14 @@ import EmailProvider from "next-auth/providers/email";
 
 export const options: NextAuthOptions = {
 	providers: [
-		GitHubProvider({
-			clientId: process.env.GITHUB_ID as string,
-			clientSecret: process.env.GITHUB_SECRET as string,
-		}),
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID as string,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
 		}),
+		EmailProvider({
+			server: process.env.EMAIL_SERVER,
+			from: process.env.EMAIL_FROM
+		  }),
 		/*CredentialsProvider({
 			name: "Credentials",
 			credentials : {
