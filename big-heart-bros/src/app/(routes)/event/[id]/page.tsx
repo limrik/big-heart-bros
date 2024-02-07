f93c55ed2ade19974a1a9ba0a49ecbb8c0e15f40
Dayn <daynangzj@gmail.com>
2024-02-07 18:39:53 +0800

    Draft events page

--- big-heart-bros/src/app/(routes)/event/[id]/page.tsx
+++ big-heart-bros/src/app/(routes)/event/[id]/page.tsx
@@ -3,16 +3,21 @@
 import { useState, useEffect } from "react";
 import { EventType, Skills, EventStatus } from "@prisma/client";
 import Navbar from "../../../../components/navbar";
+import backgroundImage from "../../../assets/bigathearts2.png";
+import EventDetails from "../../../../components/event-details";
 
 interface Event {
   id: string;
   name: string;
   description: string;
   capacity: number;
+  location: string;
   type: EventType;
   registrationDeadline: Date;
   startDate: Date;
+  startTime: Date;
   endDate: Date;
+  endTime: Date;
   skills: Skills[];
   createdAt: Date;
   posterId: string;
@@ -51,14 +56,31 @@ export default function Page({ params }: { params: { id: string } }) {
 
   return (
     <div>
-      <Navbar></Navbar>
-      <div>Event Name: {event?.name}</div>
-      <div>
-        Users
+      <div
+        className="bg-no-repeat bg-cover h-[450px] w-full"
+        style={{ backgroundImage: `url(${backgroundImage.src})` }}
+      >
+        <div className="bg-gray-900/40 absolute top-0 left-0 w-full h-[450px]">
+          <Navbar />
+          <div className="absolute top-[145px] w-full h-2/3 flex flex-col justify-center text-white">
+            <div>
+              {" "}
+              <div className="flex justify-center my-4 items-center pt-64">
+                <EventDetails
+                  eventName={event?.name}
+                  startDate={event?.startDate}
+                  endDate={event?.endDate}
+                  location={event?.location}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      {users.map((user, index) => (
-        <div key={user.id}>{index + 1}. {user.name}</div>
-      ))}
+      <div className="pt-28 mx-20">
+        <p className="font-semibold text-xl underline py-4">Event Details </p>
+        <p>{event?.description}</p>
+      </div>
     </div>
   );
 }
