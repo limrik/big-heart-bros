29ee0ff82455d56ffa1322e5730a0847d7bc92b6
Dayn <daynangzj@gmail.com>
2024-02-08 03:06:08 +0800

    Update events page

--- big-heart-bros/src/components/event-feedback-modal.tsx
+++ big-heart-bros/src/components/event-feedback-modal.tsx
@@ -26,31 +26,26 @@ const EventFeedbackModal: React.FC<EventFeedbackModalProps> = ({
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-filter backdrop-blur-sm">
-      <div className="bg-white rounded-lg shadow-lg p-6">
-        <button
-          onClick={onClose}
-          className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 focus:outline-none"
-        >
-          Close
-        </button>
-        <h2 className="text-lg font-semibold mb-4">Event Feedback</h2>
-        <p>
-          <strong>User ID:</strong> {userId}
-        </p>
-        <p>
-          <strong>User Name:</strong> {userName}
-        </p>
+    <div className="fixed inset-0 z-50i flex items-center justify-center backdrop-filter backdrop-blur-sm w-full">
+      <div className="bg-white rounded-lg shadow-lg p-6 w-1/2">
+        <h2 className="text-lg font-semibold mb-4">Feedback for {userName} </h2>
         <textarea
           value={feedback}
           onChange={(e) => setFeedback(e.target.value)}
           placeholder="Enter your feedback here..."
           className="w-full h-32 border-gray-300 rounded-md resize-none mt-4 px-3 py-2 focus:outline-none focus:ring focus:border-blue-300"
         />
-        <div className="mt-4 flex justify-end">
+        <div className="mt-4 flex justify-end gap-2">
+          {" "}
+          <button
+            onClick={onClose}
+            className="px-4 py-2 bg-gray-400 text-white rounded-md hover:bg-gray-500 focus:outline-none"
+          >
+            Close
+          </button>
           <button
             onClick={handleSubmit}
-            className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none"
+            className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-700 focus:outline-none"
           >
             Submit Feedback
           </button>
