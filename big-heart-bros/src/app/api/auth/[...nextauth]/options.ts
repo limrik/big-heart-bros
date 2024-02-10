364ec21447a79c2fb6b35d2b0a7ef910228f4cd9
smellystank <ryan.peh.wye.kin.9795@gmail.com>
2024-02-10 16:00:43 +0800

    auth test

--- big-heart-bros/src/app/api/auth/[...nextauth]/options.ts
+++ big-heart-bros/src/app/api/auth/[...nextauth]/options.ts
@@ -40,7 +40,8 @@ export const options: NextAuthOptions = {
 				}
 			}
 		}) */
-	],
+	]
+	,secret: process.env.NEXTAUTH_SECRET,
 	adapter: SupabaseAdapter({
 		url: process.env.NEXT_PUBLIC_SUPABASE_URL as string,
 		secret: process.env.SUPABASE_SERVICE_ROLE_KEY as string,
